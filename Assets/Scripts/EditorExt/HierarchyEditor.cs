e3f37b8e19cce9545b224b9e973b14e7ac558dda
Boris shishov <borisshishov@gmail.com>
2017-05-16 23:09:58 +0300

    Heirarchy editor alignment

--- Assets/Scripts/EditorExt/HierarchyEditor.cs
+++ Assets/Scripts/EditorExt/HierarchyEditor.cs
@@ -14,13 +14,26 @@ namespace Assets.Scripts.EditorExt
             public Species Species;
         }
 
-        Vector2 _scrollPosition = Vector2.zero;
+        public enum Alignment
+        {
+            ByMutation,
+            ByFood,
+            ByEnemy
+        }
+
+        private Vector2 _scrollPosition = Vector2.zero;
+        private bool _showMutations = true;
+        private bool _showEnemies = false;
+        private bool _showFood = false;
         private readonly Rect _canvas = new Rect(0, 0, 2000, 2000);
         private SpeciesWindow[] _nodes;
         private const int IconWidth = 50;
         private const int IconHeight = 50;
         private const int Padding = 10;
         private const int VSpace = 20;
+        private readonly Color _mutationConnectionColor = Color.black;
+        private readonly Color _enemyConnectionColor = new Color(0.8f, 0f, 0f);
+        private readonly Color _feedConnectionColor = new Color(0, 0.8f, 0f);
 
         [MenuItem("Window/Hierarchy editor")]
         static void ShowEditor()
@@ -33,7 +46,6 @@ namespace Assets.Scripts.EditorExt
         public void Init()
         {
             var guids = AssetDatabase.FindAssets("t:Species", new string[]{ "Assets/Resources" });
-            Debug.LogFormat("Found {0} assets", guids.Length);
 
             var species = new List<Species>();
             foreach (var guid in guids)
@@ -57,52 +69,89 @@ namespace Assets.Scripts.EditorExt
                 _nodes[i] = w;
             }
 
-            Placing();
+            Placing(Alignment.ByMutation);
+        }
+
+        IEnumerable<SpeciesWindow> IterateChildren(SpeciesWindow w, Alignment alignment)
+        {
+            if (alignment == Alignment.ByMutation)
+            {
+                foreach (var mutation in w.Species.Mutations)
+                {
+                    if (mutation.Target != null)
+                    {
+                        var childWindow = _nodes.FirstOrDefault(n => n.Species == mutation.Target);
+                        if(childWindow != null)
+                            yield return childWindow;
+                    }
+                }
+            }
+
+            if (alignment == Alignment.ByEnemy)
+            {
+                foreach (var enemy in w.Species.Enemies)
+                {
+                    if (enemy != null)
+                    {
+                        var childWindow = _nodes.FirstOrDefault(n => n.Species == enemy);
+                        if (childWindow != null)
+                            yield return childWindow;
+                    }
+                }
+            }
+
+            if (alignment == Alignment.ByFood)
+            {
+                foreach (var feedGroup in w.Species.Feed)
+                {
+                    if (feedGroup != null)
+                    {
+                        foreach (var targetNode in _nodes)
+                        {
+                            if (targetNode.Species != null && targetNode.Species.IsInGroup(feedGroup))
+                                yield return targetNode;
+                        }
+                    }
+                }
+            }
         }
         
-        int PlaceNodes(SpeciesWindow window, int startX, int y, List<SpeciesWindow> placed)
+        int PlaceNodes(SpeciesWindow window, int startX, int y, List<SpeciesWindow> placed, Alignment alignment)
         {
             var x = startX;
             window.Window.position = new Vector2(
                 Padding + x * (IconWidth + 4 * Padding),
-                Padding + y * (IconHeight + 10 * Padding));
+                Padding * 2 + y * (IconHeight + 10 * Padding));
             placed.Add(window);
 
             var i = 0;
-            
-            foreach (var mutation in window.Species.Mutations)
+
+            foreach (var child in IterateChildren(window, alignment))
             {
-                if (mutation.Target != null)
+                if (!placed.Contains(child))
                 {
-                    var childWindow = _nodes.FirstOrDefault(n => n.Species == mutation.Target);
-                    if (!placed.Contains(childWindow))
-                    {
-                        x = PlaceNodes(childWindow, x, y + 1, placed);
-                        x += 1;
-                        i++;
-                    }
+                    x = PlaceNodes(child, x, y + 1, placed, alignment);
+                    x += 1;
+                    i++;
                 }
             }
+
             if(i > 0)
                 return x - 1;
             return x;
         }
 
-        void Placing()
+        void Placing(Alignment alignment)
         {
             var root = new List<SpeciesWindow>(_nodes.ToArray());
 
             // Remove non-root elements
             foreach (var node in _nodes)
             {
-                foreach (var mutation in node.Species.Mutations)
+                foreach (var child in IterateChildren(node, alignment))
                 {
-                    if (mutation.Target != null)
-                    {
-                        var child = root.FirstOrDefault(c => c.Species == mutation.Target);
-                        if (child != null)
-                            root.Remove(child);
-                    }
+                    if (root.Contains(child))
+                        root.Remove(child);
                 }
             }
 
@@ -110,7 +159,8 @@ namespace Assets.Scripts.EditorExt
             var x = 0;
             foreach (var node in root)
             {
-                x = PlaceNodes(node, x, 0, placed);
+                x = PlaceNodes(node, x, 0, placed, alignment);
+                x += 1;
             }
         }
 
@@ -121,12 +171,27 @@ namespace Assets.Scripts.EditorExt
 
             foreach (var node in _nodes)
             {
-                foreach (var mutation in node.Species.Mutations)
+                if (_showMutations)
                 {
-                    if (mutation.Target != null)
+                    foreach (var child in IterateChildren(node, Alignment.ByMutation))
                     {
-                        var targetNode = _nodes.FirstOrDefault(n => n.Species.Equals(mutation.Target));
-                        DrawNodeCurve(node.Window, targetNode.Window);
+                        DrawNodeCurve(node.Window, child.Window, _mutationConnectionColor);
+                    }
+                }
+
+                if (_showEnemies)
+                {
+                    foreach (var child in IterateChildren(node, Alignment.ByEnemy))
+                    {
+                        DrawNodeCurve(node.Window, child.Window, _enemyConnectionColor);
+                    }
+                }
+
+                if (_showFood)
+                {
+                    foreach (var child in IterateChildren(node, Alignment.ByFood))
+                    {
+                        DrawNodeCurve(node.Window, child.Window, _feedConnectionColor);
                     }
                 }
             }
@@ -134,12 +199,7 @@ namespace Assets.Scripts.EditorExt
 
         void OnGUI()
         {
-            if (GUI.Button(new Rect(position.width - 80, 10, 50, 20), "Reload"))
-            {
-                Init();
-            }
-
-            _scrollPosition = GUI.BeginScrollView(new Rect(0, 0, position.width, position.height), _scrollPosition, _canvas);
+            _scrollPosition = GUI.BeginScrollView(new Rect(0, 16, position.width, position.height - 16), _scrollPosition, _canvas);
             DrawConnections();
 
             BeginWindows();
@@ -151,12 +211,42 @@ namespace Assets.Scripts.EditorExt
                     _nodes[index].Window = GUI.Window(index, node.Window, DrawNodeWindow, node.Species.name);
                 }
             }
-            else
-            {
-                Debug.LogWarning("Nodes is null");
-            }
             EndWindows();
             GUI.EndScrollView();
+
+
+            GUILayout.BeginHorizontal(EditorStyles.toolbar);
+            DrawToolbar();
+            GUILayout.EndHorizontal();
+        }
+
+        void DrawToolbar()
+        {
+            if (GUILayout.Button("Reload", EditorStyles.toolbarButton))
+            {
+                Init();
+            }
+
+            _showMutations = GUILayout.Toggle(_showMutations, "Mutations", EditorStyles.toolbarButton);
+            _showEnemies = GUILayout.Toggle(_showEnemies, "Enemies", EditorStyles.toolbarButton);
+            _showFood = GUILayout.Toggle(_showFood, "Food", EditorStyles.toolbarButton);
+
+            if (GUILayout.Button("Align by mutation", EditorStyles.toolbarButton))
+            {
+                Placing(Alignment.ByMutation);
+            }
+
+            if (GUILayout.Button("Align by enemies", EditorStyles.toolbarButton))
+            {
+                Placing(Alignment.ByEnemy);
+            }
+
+            if (GUILayout.Button("Align by food", EditorStyles.toolbarButton))
+            {
+                Placing(Alignment.ByFood);
+            }
+
+            GUILayout.FlexibleSpace();
         }
 
         void DrawNodeWindow(int index)
@@ -170,17 +260,17 @@ namespace Assets.Scripts.EditorExt
             GUI.DragWindow();
         }
 
-        void DrawNodeCurve(Rect start, Rect end)
+        void DrawNodeCurve(Rect start, Rect end, Color color)
         {
             var startPos = new Vector3(start.x + start.width / 2, start.y + start.height, 0);
             var endPos = new Vector3(end.x + end.width / 2, end.y - 10, 0);
             var startTan = startPos + Vector3.up * 50;
             var endTan = endPos + Vector3.down * 50;
-            
-            Handles.DrawBezier(startPos, endPos, startTan, endTan, Color.black, null, 1.5f);
+
+            Handles.DrawBezier(startPos, endPos, startTan, endTan, color, null, 1.5f);
 
             Handles.color = Color.black;
-            Handles.ConeCap(0, endPos + new Vector3(0, 5, -10f), Quaternion.Euler(-90, 0, 0), 10f);
+            Handles.ConeCap(0, endPos + new Vector3(0, 5, -5f), Quaternion.Euler(-90, 0, 0), 10f);
         }
     }
 }
